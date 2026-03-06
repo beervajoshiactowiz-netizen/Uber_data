6a20c9f0d54c2c67f3ac23e778f9d8f88a4cd747
beervajoshiactowiz-netizen <beervajoshi.actowiz@gmail.com>
2026-03-06 14:06:19 +0530

    add function that reads the gzip files range wise

--- utils.py
+++ utils.py
@@ -2,20 +2,21 @@ import os
 import json
 import gzip
 
-
 def read_files(path: str):
     with open(path,"r") as f:
         data=json.load(f)
         return data
 
-def read_files_zip(path: str):
-    try:
-        files = os.listdir(path)
-        for file in files:
-            filename = os.path.join(path, file)
-            content = gzip.open(filename).read()
-            yield json.loads(content)
-    except Exception as e:
-        print("Error in func:", read_files.__name__, '\nError: ', e)
+def read_gzip_range(FOLDER, start, end):
+    files = sorted(os.listdir(FOLDER))
 
+    for file in files[start:end]:
+        path = os.path.join(FOLDER, file)
 
+        try:
+            with gzip.open(path, "rt", encoding="utf-8") as f:
+                data=json.load(f)
+                yield data
+
+        except Exception as e:
+            print("File error:", file, e)
