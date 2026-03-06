5ca2c7d10488849d5784309091022e498f1d0623
beervajoshiactowiz-netizen <beervajoshi.actowiz@gmail.com>
2026-03-06 14:05:05 +0530

    inserted the extracted data by adding batch size

--- db_config.py
+++ db_config.py
@@ -1,8 +1,6 @@
 import mysql.connector
 import json
 
-
-
 def make_connection():
     conn = mysql.connector.connect(
         host='localhost',
@@ -10,18 +8,17 @@ def make_connection():
         password='actowiz',
         database='RestaurantData'
     )
-
     return conn
 
 def create(table_name: str):
     q = f"""
         CREATE TABLE IF NOT EXISTS {table_name} (
             RestaurantName  varchar(255),
-            RestaurantId varchar(255),
+            RestaurantId varchar(255) unique,
             phoneNo varchar(100),
-            Address  varchar(100),
-            street  varchar(255),
-            city  varchar(100),
+            Address  varchar(500),
+            street  varchar(500),
+            city  varchar(500),
             country varchar(100),
             Postalcode varchar(100) ,
             region varchar(100),
@@ -40,20 +37,29 @@ def create(table_name: str):
     conn.commit()
     conn.close()
 
+def insert_batch(table_name, data_list):
+    if not data_list:
+        return
 
-def insert_into_db(table_name: str, data: dict):
-    for k, v in data.items():
-        if isinstance(v, (list, dict)):
-            data[k] = json.dumps(v)
-    cols = ",".join(list(data.keys()))
-    vals = "".join([len(data.keys()) * '%s,']).strip(',')
-    q = f"INSERT INTO {table_name} ({cols}) VALUES ({vals})"
     conn = make_connection()
     cursor = conn.cursor()
-    cursor.execute(q, tuple(data.values()))
+
+    for data in data_list:
+        data["Cuisines"] = json.dumps(data.get("Cuisines"))
+        data["supportedDiningMode"] = json.dumps(data.get("supportedDiningMode"))
+        data["MenuItems"] = json.dumps(data.get("MenuItems"))
+        data["timing"]=json.dumps(data.get("timing"))
+
+    cols = ",".join(data_list[0].keys())
+
+    vals = ",".join(["%s"] * len(data_list[0]))
+
+    q = f"INSERT INTO {table_name} ({cols}) VALUES ({vals})"
+
+    rows = [tuple(d.values()) for d in data_list]
+
+    cursor.executemany(q, rows)
+
     conn.commit()
     conn.close()
 
-
-if __name__ == '__main__':
-    pass
