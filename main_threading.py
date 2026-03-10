6e905c2569e22382227d6616badc51eeeeee1c41
beervajoshiactowiz-netizen <beervajoshi.actowiz@gmail.com>
2026-03-10 19:12:27 +0530

    added logging

--- main_threading.py
+++ main_threading.py
@@ -1,6 +1,7 @@
 import sys
 import time
 import threading
+import logging
 from utils import read_gzip_range
 from parser import parser
 from db_config import create, insert_batch,make_connection
@@ -8,6 +9,17 @@ from db_config import create, insert_batch,make_connection
 
 FOLDER = r"C:\Users\beerva.joshi\PycharmProjects\new_json\PDP\PDP"
 TABLE_NAME = "RestaurantData"
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s | %(levelname)s | %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    handlers=[
+        logging.FileHandler("ubereat.log"),   # saves to file
+    ]
+)
+logger = logging.getLogger(__name__)
 
 BATCH_SIZE = 2000
 lock            = threading.Lock()
@@ -37,14 +49,14 @@ def main(thread_id,start, end):
             insert_batch(TABLE_NAME, batch,conn,cursor)
 
             total += len(batch)
-            print(f"[Thread-{thread_id}] Inserted: {total}")
+            logger.info(f"[Thread-{thread_id}] Inserted: {total}")
             batch = []
     if batch:
         insert_batch(TABLE_NAME, batch,conn,cursor)
         total += len(batch)
     cursor.close()
     conn.close()
-    print(f"[Thread-{thread_id}] Finished. Inserted: {total} | Failed: {failed}")
+    logger.info(f"[Thread-{thread_id}] Finished. Inserted: {total} | Failed: {failed}")
 
     with lock:
         grand_total += total
@@ -58,16 +70,16 @@ if __name__ == "__main__":
     start_time = time.time()
     threads=[]
     files=122888
-    step=20000
+    step=25000
     for i in range(0,files,step):
         last=min(i+step,files)
         t=threading.Thread(target=main,args=(i//step+1,i,last))
         threads.append(t)
         t.start()
-        print(f"[Thread-{i // step + 1}] Started -> files {i} to {last}")
+        logger.info(f"[Thread-{i // step + 1}] Started -> files {i} to {last}")
 
     for i in threads:
         i.join()
 
 
-    print("Total Time:", time.time() - start_time)
+    logger.info(f"Time: {time.time() - start_time:.2f} sec")
